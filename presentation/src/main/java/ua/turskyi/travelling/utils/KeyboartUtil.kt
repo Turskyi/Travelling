160f2f7e83cb92cf64f88dcbbc29e65bc451ed09
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-05-19 11:10:00 +0300

    fixed bug with opening soft keyboard

--- presentation/src/main/java/ua/turskyi/travelling/utils/KeyboartUtil.kt
+++ presentation/src/main/java/ua/turskyi/travelling/utils/KeyboartUtil.kt
@@ -4,24 +4,21 @@ import android.app.Activity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 
 fun AppCompatActivity.hideKeyboard() {
     val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     /* Finds the currently focused view, so we can grab the correct window token from it. */
     var view = currentFocus
-    /* If no view currently does not have a focus, create a new one, just so we can grab a window token from it */
+    /* If no view currently does not have a focus, create a new one, just so we can grab a window
+     token from it */
     view ?: run {
         view = View(this)
     }
     imm.hideSoftInputFromWindow(view?.windowToken, 0)
+    view?.clearFocus()
 }
 
 fun AppCompatActivity.showKeyboard() {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
-}
-
-fun Fragment.hideKeyboard() {
-    (activity as AppCompatActivity).hideKeyboard()
 }
