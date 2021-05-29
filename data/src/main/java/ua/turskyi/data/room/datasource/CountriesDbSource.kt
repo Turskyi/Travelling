0b1146e41bbf1999739f1e9b452e9822290d69f0
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-05-29 13:47:00 +0300

    update ci and kotlin

--- data/src/main/java/ua/turskyi/data/room/datasource/CountriesDbSource.kt
+++ data/src/main/java/ua/turskyi/data/room/datasource/CountriesDbSource.kt
@@ -14,12 +14,12 @@ class CountriesDbSource(private val countriesDao: CountriesDao) {
     fun getCountNotVisitedCountries() = countriesDao.getNumNotVisitedCountries()
     fun getVisitedLocalCountriesFromDb() = countriesDao.getVisitedCountries()
     fun getCities():List<CityEntity> = countriesDao.getCities()
-    suspend fun insertCountry(countryEntity: CountryEntity) = countriesDao.insertCountry(countryEntity)
+    fun insertCountry(countryEntity: CountryEntity) = countriesDao.insertCountry(countryEntity)
     fun insertAllCountries(countries: List<CountryEntity>) =
         countriesDao.insertAllCountries(countries)
 
     fun insertCity(cityEntity: CityEntity) = countriesDao.insertCity(cityEntity)
     fun removeCity(city: CityEntity) = countriesDao.delete(city)
-    suspend fun removeCitiesByCountry(parentId: Int) = countriesDao.removeCitiesByCountry(parentId)
-    suspend fun updateSelfie(id: Int, selfie: String) = countriesDao.updateSelfie(id, selfie)
+    fun removeCitiesByCountry(parentId: Int) = countriesDao.removeCitiesByCountry(parentId)
+    fun updateSelfie(id: Int, selfie: String) = countriesDao.updateSelfie(id, selfie)
 }
