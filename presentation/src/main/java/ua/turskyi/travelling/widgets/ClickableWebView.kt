2d23e6c8e049ead15ecf99f8a351a1732a73426f
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-07-11 13:51:38 +0300

    add ListenableWebView.kt

--- presentation/src/main/java/ua/turskyi/travelling/widgets/ClickableWebView.kt
+++ presentation/src/main/java/ua/turskyi/travelling/widgets/ClickableWebView.kt
@@ -1,6 +1,5 @@
 package ua.turskyi.travelling.widgets
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
@@ -9,9 +8,10 @@ import java.util.*
 
 class ClickableWebView : WebView {
     companion object {
-        const val MAX_CLICK_DURATION = 200
-        var startClickTime: Long = 0
+        private const val MAX_CLICK_DURATION = 200
     }
+
+    private var startClickTime: Long = 0
 
     constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(
         context,
@@ -22,20 +22,28 @@ class ClickableWebView : WebView {
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context) : super(context)
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 startClickTime = Calendar.getInstance().timeInMillis
             }
             MotionEvent.ACTION_UP -> {
-                val clickDuration: Long =
-                    Calendar.getInstance().timeInMillis - startClickTime
+                val clickDuration: Long = Calendar.getInstance().timeInMillis - startClickTime
                 if (clickDuration < MAX_CLICK_DURATION) {
+                    /* For this particular app we want the main work to happen
+                     * on ACTION_UP rather than ACTION_DOWN. So this is where
+                     * we will call performClick(). */
                     super.performClick()
                 }
             }
         }
+        return true
+    }
+
+    /* Because we call this from onTouchEvent, this code will be executed for both
+     * normal touch events and for when the system calls this using Accessibility */
+    override fun performClick(): Boolean {
+        super.performClick()
         return true
     }
 }
