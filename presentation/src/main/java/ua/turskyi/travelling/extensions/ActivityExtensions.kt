6e21e4dcdf267d47fa97f3bb90413a6589e296e0
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-12-19 20:32:10 -0500

    upgrade firebase-analytics

--- presentation/src/main/java/ua/turskyi/travelling/extensions/ActivityExtensions.kt
+++ presentation/src/main/java/ua/turskyi/travelling/extensions/ActivityExtensions.kt
@@ -1,18 +1,15 @@
-package ua.turskyi.travelling.extensions
+package ua.turskyi.travelling.utils.extensions
 
 import android.app.Activity
 import android.graphics.Insets
 import android.os.Build
 import android.util.DisplayMetrics
-import android.view.ViewGroup
 import android.view.WindowInsets
 import android.view.WindowMetrics
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
-import ua.turskyi.travelling.R
 import ua.turskyi.travelling.common.view.InfoDialog
-import ua.turskyi.travelling.features.home.view.ui.SyncDialog
 
 fun AppCompatActivity.openInfoDialog(info: String) {
     val infoDialog = InfoDialog.newInstance(info)
@@ -24,36 +21,28 @@ fun AppCompatActivity.openInfoDialog(@StringRes info: Int) {
     infoDialog.show(this.supportFragmentManager, "info dialog")
 }
 
-fun AppCompatActivity.openSyncDialog(@StringRes info: Int) {
-    val infoDialog = SyncDialog.newInstance(getString(info))
-    infoDialog.show(this.supportFragmentManager, "sync dialog")
+fun Activity.getScreenWidth(): Int {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val windowMetrics: WindowMetrics = windowManager.currentWindowMetrics
+        val insets: Insets = windowMetrics.windowInsets
+            .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+        windowMetrics.bounds.width() - insets.left - insets.right
+    } else {
+        val displayMetrics = DisplayMetrics()
+        @Suppress("DEPRECATION")
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        displayMetrics.widthPixels
+    }
 }
 
-fun Activity.getScreenWidth() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-    val windowMetrics: WindowMetrics = windowManager.currentWindowMetrics
-    val insets: Insets = windowMetrics.windowInsets
-        .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
-    windowMetrics.bounds.width() - insets.left - insets.right
-} else {
-    val displayMetrics = DisplayMetrics()
-    @Suppress("DEPRECATION")
-    windowManager.defaultDisplay.getMetrics(displayMetrics)
-    displayMetrics.widthPixels
+fun Activity.toastLong(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
 }
 
-fun Activity.toastLong(@StringRes msg: Int) =
-    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-
-fun Activity.toastLong(msg: String) =
-    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-
-fun Activity.toast(@StringRes message: Int) =
+fun Activity.toast(@StringRes message: Int) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
 
-fun Activity.toast(message: String) =
+fun Activity.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-
-fun Activity.showSnackbar(@StringRes message: Int) {
-    val viewGroup = (findViewById<ViewGroup>(R.id.content)).getChildAt(0) as ViewGroup
-    viewGroup.showSnackBar(message)
 }
