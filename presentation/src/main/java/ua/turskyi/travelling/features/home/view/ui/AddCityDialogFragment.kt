627f3dcb8e44e8d116792cb2d5ef3515684afdda
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2022-04-18 17:05:25 -0400

    add padding bottom to the country list

--- presentation/src/main/java/ua/turskyi/travelling/features/home/view/ui/AddCityDialogFragment.kt
+++ presentation/src/main/java/ua/turskyi/travelling/features/home/view/ui/AddCityDialogFragment.kt
@@ -242,8 +242,6 @@ class AddCityDialogFragment : DialogFragment() {
                 val cityChanged: String = addressesChanged.first().locality
                 editText.setText(cityChanged)
             }
-
-            override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
             override fun onProviderEnabled(provider: String) {}
             override fun onProviderDisabled(provider: String) {}
         }
